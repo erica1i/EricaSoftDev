134caded0ce0c677cc5dd1b30f27289d9b2f5b02
erica1i <113399531+erica1i@users.noreply.github.com>
2022-09-21 21:08:47 -0400

    Create monkey_trouble.py
    finished comments

---
+++ 03_py/warmup1/monkey_trouble.py
@@ -0,0 +1,15 @@
+'''
+Return true if both or neither are smiling. 
+
+monkey_trouble(True, True) → True	True	
+monkey_trouble(False, False) → True	True	
+monkey_trouble(True, False) → False	False	
+monkey_trouble(False, True) → False	False	
+
+'''
+def monkey_trouble(a_smile, b_smile):
+  if (a_smile) == (b_smile):
+    return True
+    
+  else:
+    return False
