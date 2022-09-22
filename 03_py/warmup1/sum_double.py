1851edc670835753280200147e00f84cb771b3af
erica1i <113399531+erica1i@users.noreply.github.com>
2022-09-21 22:03:03 -0400

    Update sum_double.py

--- 03_py/warmup1/sum_double.py
+++ 03_py/warmup1/sum_double.py
@@ -2,11 +2,6 @@
 Return the sum of the given two ints...
 Unless the two values are the same, then return double their sum.
 
-sum_double(1, 2) → 3
-sum_double(3, 2) → 5
-sum_double(2, 2) → 8
-sum_double(-1, 0) → -1
-
 '''
 
 def sum_double(a, b):
@@ -14,3 +9,8 @@ def sum_double(a, b):
     return (a+b)*2  #returned double
     
   return a + b
+
+print(sum_double(1, 2)) #→ 3
+print(sum_double(3, 2)) #→ 5
+print(sum_double(2, 2)) #→ 8
+print(sum_double(-1, 0)) #→ -1
