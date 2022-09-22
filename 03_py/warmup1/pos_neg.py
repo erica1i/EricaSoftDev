770e538d9d2a79184d996706b153eb5841bd2ba1
erica1i <113399531+erica1i@users.noreply.github.com>
2022-09-21 21:41:09 -0400

    added comments

--- 03_py/warmup1/pos_neg.py
+++ 03_py/warmup1/pos_neg.py
@@ -9,13 +9,13 @@ pos_neg(-4, -5, False) → False
 '''
 
 def pos_neg(a, b, negative):
-  if negative == True:
-    if a < 0 and b < 0:
+  if negative == True:  #triggers second req since parameter "negative" is True
+    if a < 0 and b < 0: #checked for the second req where you return True only if both are negative
       return True
     else:
       return False
       
-  if (a > 0 and b > 0) or (a < 0 and b < 0):
+  if (a > 0 and b > 0) or (a < 0 and b < 0): #checked if both are pos or both are neg
     return False
   
   return True
