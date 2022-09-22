d157e6235f450cd129021435658f1d8e49b353aa
erica1i <113399531+erica1i@users.noreply.github.com>
2022-09-21 21:28:37 -0400

    Update makes10.py

--- 03_py/warmup1/makes10.py
+++ 03_py/warmup1/makes10.py
@@ -5,6 +5,9 @@ makes10(9, 10) → True
 makes10(9, 9) → False	
 makes10(1, 9) → True	
 makes10(10, 1) → True	
+
+DISCO: Similar to Java, you do not need an else statement to just go directly into the 
+statement if the "if" statement isn't triggered. 
 '''
 
 def makes10(a, b):
