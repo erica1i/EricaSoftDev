4e6a88bd9c98ba654114c5ef477f6b44cdfc3f6a
erica1i <113399531+erica1i@users.noreply.github.com>
2022-09-21 21:33:24 -0400

    added comments & DISCO

--- 03_py/warmup1/missing_char.py
+++ 03_py/warmup1/missing_char.py
@@ -6,6 +6,8 @@ missing_char('kitten', 0) → 'itten'
 missing_char('kitten', 4) → 'kittn'
 missing_char('Hi', 0) → 'i'
 
+DISCO: Very cool discovery here. To get the substring of a string from index n to the end, 
+all you need to do is [n:]. Python automatically assumes that it goes to the last index. 
 '''
 def missing_char(str, n):
-  return str[0:n] + str[n+1:]
+  return str[0:n] + str[n+1:] #took substring before index n and the substring after it & combined them together
