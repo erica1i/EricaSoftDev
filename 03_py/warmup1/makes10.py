4c994c1dee6f50f62cbbbe7fa37927558571e6c3
erica1i <113399531+erica1i@users.noreply.github.com>
2022-09-21 21:56:58 -0400

    Update makes10.py

--- 03_py/warmup1/makes10.py
+++ 03_py/warmup1/makes10.py
@@ -1,11 +1,6 @@
 '''
 Given 2 ints, a and b, return True if one if them is 10 or if their sum is 10.
 
-makes10(9, 10) → True	
-makes10(9, 9) → False	
-makes10(1, 9) → True	
-makes10(10, 1) → True	
-
 DISCO: Similar to Java, you do not need an else statement to just go directly into the 
 statement if the "if" statement isn't triggered. 
 '''
@@ -14,3 +9,8 @@ def makes10(a, b):
   if a == 10 or b == 10 or b + a == 10: #returned True if one or their sum is 10. 
     return True
   return False
+
+print(makes10(9, 10)) #→ True	
+print(makes10(9, 9)) #→ False	
+print(makes10(1, 9)) #→ True	
+print(makes10(10, 1)) #→ True	
