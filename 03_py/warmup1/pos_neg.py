7c5fdc6bfea72b5382f6613ee403304635a23362
erica1i <113399531+erica1i@users.noreply.github.com>
2022-09-21 22:01:10 -0400

    Update pos_neg.py

--- 03_py/warmup1/pos_neg.py
+++ 03_py/warmup1/pos_neg.py
@@ -2,10 +2,6 @@
 Given 2 int values, return True if one is negative and one is positive. 
 Except if the parameter "negative" is True, then return True only if both are negative.
 
-pos_neg(1, -1, False) → True
-pos_neg(-1, 1, False) → True
-pos_neg(-4, -5, True) → True
-pos_neg(-4, -5, False) → False
 '''
 
 def pos_neg(a, b, negative):
@@ -19,3 +15,8 @@ def pos_neg(a, b, negative):
     return False
   
   return True
+
+print(pos_neg(1, -1, False)) #→ True
+print(pos_neg(-1, 1, False)) #→ True
+print(pos_neg(-4, -5, True)) #→ True
+print(pos_neg(-4, -5, False)) #→ False
