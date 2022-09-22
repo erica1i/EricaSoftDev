a3818905eda5ad4b3082734cee192d5a34885ee5
erica1i <113399531+erica1i@users.noreply.github.com>
2022-09-21 21:59:52 -0400

    Update not_string.py

--- 03_py/warmup1/not_string.py
+++ 03_py/warmup1/not_string.py
@@ -2,11 +2,6 @@
 Given a string, return a new string where "not " has been added to the front. 
 However, if the string already begins with "not", return the string unchanged.
 
-not_string('candy') → 'not candy'
-not_string('x') → 'not x'
-not_string('not bad') → 'not bad'
-not_string('bad') → 'not bad'
-
 DISCO: First DISCO of the warmup! You can combine strings just by using the + sign. 
 
 '''
@@ -15,3 +10,8 @@ def not_string(str):
   if str[:3] == "not": #if string began with not, then it would be unchanged. 
     return str
   return "not" + str 
+
+print(not_string('candy')) #→ 'not candy'
+print(not_string('x')) #→ 'not x'
+print(not_string('not bad')) #→ 'not bad'
+print(not_string('bad')) #→ 'not bad'
