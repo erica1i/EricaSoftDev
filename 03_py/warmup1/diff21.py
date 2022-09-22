94a48110c0358f09ab878af008e5583d2bd09d86
erica1i <113399531+erica1i@users.noreply.github.com>
2022-09-21 21:54:52 -0400

    Update diff21.py

--- 03_py/warmup1/diff21.py
+++ 03_py/warmup1/diff21.py
@@ -1,11 +1,6 @@
 '''
 Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
 
-diff21(0) → 21	
-diff21(1) → 20	
-diff21(2) → 19	
-diff21(-1) → 22	
-
 DISCO: Learned something cool here. The abs function in Python as it is in Java. 
 abs(a-b) gives you the absolute value of the difference between a-b. Swag. 
 
@@ -17,3 +12,8 @@ def diff21(n):
   
   else:
     return abs(21-n) #found absolute difference
+
+print(diff21(0)) # → 21	
+print(diff21(1)) # → 20	
+print(diff21(2)) # → 19	
+print(diff21(-1))# → 22	
