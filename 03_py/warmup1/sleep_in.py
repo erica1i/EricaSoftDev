20f89c441a29af7f5d4d5e20d54deec3d4758067
erica1i <113399531+erica1i@users.noreply.github.com>
2022-09-21 22:02:24 -0400

    Update sleep_in.py

--- 03_py/warmup1/sleep_in.py
+++ 03_py/warmup1/sleep_in.py
@@ -2,12 +2,6 @@
 The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation. 
 We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
 
->>> 
-True
->>> 
-True
->>> 
-True
 '''
 
 def sleep_in(weekday, vacation):
