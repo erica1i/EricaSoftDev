d5f353bdf4557585c09a3b2813c0c8148777344e
erica1i <113399531+erica1i@users.noreply.github.com>
2022-09-21 21:27:10 -0400

    Added comments

--- 03_py/warmup1/front_back.py
+++ 03_py/warmup1/front_back.py
@@ -10,8 +10,8 @@ front_back('') → ''
 '''
 
 def front_back(str):
-  if (len(str) <= 1):
-    return str
+  if (len(str) <= 1): #if length is less or equal to 1, there is no difference between the first and last chars
+    return str        #thus, you would just return the str
   return str[len(str)-1] + str[1:len(str)-1] + str[0:1]
 
 
