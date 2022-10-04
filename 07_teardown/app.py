37ae711c58122fc92d32777ed04a4fd8c8ee87eb
erica1i <eli30@stuy.edu>
2022-10-04 15:45:39 -0400

    lets see

--- 07_teardown/app.py
+++ 07_teardown/app.py
@@ -12,11 +12,13 @@ from flask import Flask
 app = Flask(__name__) # Q0: Where have you seen similar syntax in other langs?
                           # When declaring/intiating new objects (ie Stack<Integer> = new Stack())
 
-@app.route("/") # Q1: What points of reference do you have for meaning of '/'?
+app.route("/") # Q1: What points of reference do you have for meaning of '/'?
                     # In python, '/' continues the next line to the current line
+                    # The '/' indicates going into a new route
 def hello_world():
     print(__name__) # Q2: Where will this print to? Q3: What will it print?
                     # It printed to the terminal itself and printed a new site/link.
+    
     return "No hablo queso!"  # Q4: Will this appear anywhere? How u know?
                                 # "No hablo queso!" appeared on the screen when you clicked the link printed.
                                 #  I knew this because I downloaded the Flask package and was able to print out
