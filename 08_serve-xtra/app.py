557e1b125b6327464f31d1aa7c9e9fcc7a58422c
Erica Li <admin@admins-air.lan>
2022-10-09 22:10:08 -0400

    tried some new stuff

--- 08_serve-xtra/app.py
+++ 08_serve-xtra/app.py
@@ -8,6 +8,7 @@ time spent:
 '''
 import random
 from flask import Flask
+from simple_colors import *
 
 app = Flask(__name__)
 
@@ -29,6 +30,18 @@ for i in range(2,len(temp)-2,2):
     library["Job Class"].append(temp[i])
     library["Percentage"].append(float(temp[i+1]))
 
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
 def choose_occupations():
     options = library["Job Class"]
     option_weights = library["Percentage"]
@@ -36,20 +49,22 @@ def choose_occupations():
     return o
 
 def job_lister():
-    jobs = ""
+    jobs = green('OCCUPATIONS', 'bold') + "<br/>"
     for values in library["Job Class"]:
         jobs += values + "<br/>"
+
+    print (green('OCCUPATIONS', 'bold'))
     return jobs
 
 @app.route("/")
 def hello_world():
     print(__name__)
-    printed = "TNPG: Holi Goramali: Erica (Hugo), Gordon (The Blueman) <br/> <br/>"
+    printed = "Holi Goramali: Erica (Hugo), Gordon (The Blueman)" + "<br> <br>"
     occupation = choose_occupations()
-    printed += occupation[0] + "<br/> <br/> <br/>"
+    printed += occupation[0] + "<br/> <br/>"
     printed += job_lister()
     return printed
 
 if __name__ == "__main__":  # true if this file NOT imported
-    app.debug = False        # enable auto-reload upon code change
+    app.debug = True        # enable auto-reload upon code change
     app.run()
