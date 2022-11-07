bc7a3aa9a2cbf5a9e6c034ea0fe6b03669256a2b
erica1i <eli30@stuy.edu>
2022-11-07 13:30:03 -0500

    added

--- 19_session/app.py
+++ 19_session/app.py
@@ -3,13 +3,13 @@ Henali: Erica (hugo), Henry (e), Aahan (spikes)
 19_Session
 SoftDev
 2022-11-03
-time spent: 0.5 hr
+time spent: 2.5 hr
 
 DISCO:
-QCC: 
+QCC:
 
 '''
-from flask import Flask, session, request, render_template
+from flask import Flask, session, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 app.secret_key = 'foo'
@@ -17,21 +17,26 @@ app.secret_key = 'foo'
 @app.route("/") #, methods = ['POST'])
 def login():
     if 'username' in session:
-        return render_template('welcome.html')
+        return render_template('welcome.html', username=session['username'])
     return render_template('login.html')
 
 @app.route("/welcome", methods = ['POST'])
-def welcome(): 
-    if request.form.get('username') == "Foo" and request.form.get('password') == 'Bar' :
-        session['username'] = request.form.get('username')
-        return render_template('welcome.html')
-    return render_template('login.html')
-    
+def welcome():
+    if request.method == "POST" :
+        if request.form.get('username') == "Foo" and request.form.get('password') == "Bar" :
+            session['username'] = request.form.get('username')
+            return render_template('welcome.html', username=session['username'])
+    return render_template('login.html', additional="Incorrect Username or Password")
+
 @app.route("/logout", methods = ['POST'])
 def logout():
     session.pop('username')
-    return render_template('login.html')
+    return redirect(url_for('login'))
+
+# @app.route("/test")
+# def test():
+#     return "test"
 
 if __name__ == "__main__":
     app.debug = True
-    app.run()
+    app.run(port=1026)
--- 19_session/readme.md
+++ 19_session/readme.md
@@ -1,3 +1,8 @@
 ## Notes
+DISCO:
 - To Add Value: session[key] = value
 - To Remove Value: session.pop(key)
+- redirect(url_for('login')) allows us to head to a certain page with the python script
+
+QCC:
+- 
