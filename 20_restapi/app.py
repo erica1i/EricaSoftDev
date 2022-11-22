945e5d616e2cfd01ff2be961a00d32f798cb457f
erica1i <eli30@stuy.edu>
2022-11-22 13:42:21 -0500

    slayed

--- 20_restapi/app.py
+++ 20_restapi/app.py
@@ -3,20 +3,28 @@ stevejobsghost: Verit Li (Nibbles), Erica Li (Hugo)
 K20 - REST API
 Soft Dev
 2022-11-22
-time spent:
+time spent: 0.5 hr
 '''
 
+import requests
 from flask import Flask, render_template
-import urllib.request, json
 
 app = Flask(__name__)
 
-@app.route("/") #, methods = ['GET'])
-def main(): 
-    key = (open('key_nasa.txt')).read()
-    url = "https://api.nasa.gov/planetary/apod?api_key=" + key
-    json_url = urllib.request.urlopen(url)
 
+with open('key_nasa.txt') as f:
+    key = f.read()
+
+#print(key)
+nasa_url = "https://api.nasa.gov/planetary/apod?api_key=" + key
+print(nasa_url) #prints the nasa_url with key to the console
+res = requests.get(nasa_url) 
+pic_url = json["url"] #finds the key named url in the json file 
+
+@app.route("/")
+def picture():
+    return render_template('main.html', url=pic_url)
+    
 if __name__ == "__main__":
     app.debug = True
-    app.run(port=2528)
+    app.run()
---
+++ 20_restapi/readme.md
@@ -0,0 +1,6 @@
+## DISCO 
+URL with ampersands (&), question marks (?), and equals signs (=) indicates that the URL is for GET requests 
+
+## QCC
+How would you use urllib.request module to open URLs?
+Whats the difference between Python GET request and urllib.request to open URLs? 
