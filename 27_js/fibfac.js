d92e1efd7ee7f735f347d256abb8533442da55ed
erica1i <eli30@stuy.edu>
2023-04-04 09:28:45 -0400

    ha

---
+++ 27_js/fibfac.js
@@ -0,0 +1,13 @@
+// Team Phantom Tollbooth :: Clyde Sinclair, Fierce Dragon 
+// SoftDev pd0
+// K27 -- Basic functions in JavaScript
+// 2023-04-04t
+// --------------------------------------------------
+
+
+// as a duo...
+// pair programming style,
+// implement a fact and fib fxn
+
+
+//Do whatever you think is needed. Think: S I M P L E.   Think: S M A R T.
