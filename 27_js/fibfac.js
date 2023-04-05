ff66d2eb51d4ed7d6ca0b2a04de3a7bd2854b81c
erica1i <eli30@stuy.edu>
2023-04-05 09:08:05 -0400

    slay

--- 27_js/fibfac.js
+++ 27_js/fibfac.js
@@ -1,4 +1,4 @@
-// Team Phantom Tollbooth :: Clyde Sinclair, Fierce Dragon 
+// Team iHeartAI :: Erica, Nada 
 // SoftDev pd0
 // K27 -- Basic functions in JavaScript
 // 2023-04-04t
@@ -11,3 +11,32 @@
 
 
 //Do whatever you think is needed. Think: S I M P L E.   Think: S M A R T.
+
+//Factorial Function
+function fact (n) {
+    if (n < 2)
+        return 1; 
+    else
+        return n * (fact (n -1));
+}
+
+//Fibonacci Function
+function fib (n) {
+    if (n <= 1)
+        return n; 
+
+    else
+        return fib(n-1) + (fib(n-2));
+}
+
+console.log(fact (1))
+console.log(fact (2))
+console.log(fact (3))
+console.log(fact (4))
+console.log(fact (5))
+
+console.log(fib (1))
+console.log(fib (2))
+console.log(fib (3))
+console.log(fib (4))
+console.log(fib (5))
