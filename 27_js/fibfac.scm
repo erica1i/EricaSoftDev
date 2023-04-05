9ccc60732125fe59f9300c158fd9fa9ce3f29f99
erica1i <eli30@stuy.edu>
2023-04-05 09:12:19 -0400

    slay

---
+++ 27_js/fibfac.scm
@@ -0,0 +1,26 @@
+;Team iHeartAI :: Erica, Nada
+;SoftDev pd0
+;K27 -- Basic functions in JavaScript
+;2023-04-03t
+
+;Scheme Antecedents for JS work
+
+;factorial:
+;(define fact (lamba n) (if (< n 2) 1 (* n (fact (- n 1)))))
+
+;fact(1) ;"...should be  1"
+;fact(2) ;"...should be  2"
+;fact(3) ;"...should be  6"
+;fact(4) ;"...should be  24"
+;fact(5) ;"...should be  120"
+
+
+;fib:
+;(define (fib n) (if (< n 2) n (+ (fib(- n 1) (fib(- n 2))
+
+;fib(0) ;"...should be  0"
+;fib(1) ;"...should be  1"
+;fib(2) ;"...should be  1"
+;fib(3) ;"...should be  2"
+;fib(4) ;"...should be  3"
+=================================================================
