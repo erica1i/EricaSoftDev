503d00e344043dd0c105b33bb91eba8e6c9718b6
erica1i <eli30@stuy.edu>
2023-04-24 09:28:59 -0400

    lol

---
+++ 30_jscanvas/clickdraw.js
@@ -0,0 +1,40 @@
+// Team iHeartAI: Nada Hameed, Erica Li
+// SoftDev pd2
+// K30 - canvas based on JS
+// 2023-04-24m
+// --------------------------------------------------
+
+//retrieve node in DOM via ID
+var c = document.getElementById("slate");
+
+//instantiate a CanvasRenderingContext2D object
+var ctx = c.getContext("2d");
+
+//init global state var
+var mode = "rect"; 
+
+//var toggleMode = function(e) {
+var toggleMode = e.addEventListener('click', () => {
+    console.log("toggling...");
+    e = document.getElementById("buttonToggle");
+    if (){
+
+    }
+}
+
+var drawRect = function(e) {
+    var mouseX = 
+    var mouseY = 
+    console.log("mouseclick registered at ", mouseX, mouseY);
+}
+
+//var drawCircle = function(e){
+var drawCircle = (e) => {
+    console.log("mouseclick registered at ", mouseX, mouseY);
+}
+
+//var draw = function(e){
+var draw = (e) => {
+
+}
+
